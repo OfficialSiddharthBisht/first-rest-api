f03cddb53c3e322fcbb706a9fd757c97e17304e4
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-23 22:17:13 +0530

    started builing rest api

---
+++ routes/subscribers.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+// Getting all subscribers
+router.get('/',(req ,res )=>{
+
+})
+// Getting one subscriber
+router.get('/:id',(req ,res)=>{
+
+})
+// Creating one subscriber
+router.post('/',(req , res)=>{
+
+})
+// Updating one subscriber (based on what user passes -> patch)
+router.patch('/:id',(req , res)=>{
+
+})
+// Deleting one subscriber
+router.delete('/:id',(req ,res )=>{
+
+})
+
+
+module.exports = router;
